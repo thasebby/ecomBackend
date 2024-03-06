a3c9ee0830f1eae1c00a8f2c722d963bc0a353e5
Sebastian Saenz <sebastian.saenz1203@gmail.com>
2024-03-06 13:47:30 -0500

    finished the actual index.js in the model directory

--- models/index.js
+++ models/index.js
@@ -5,13 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  // tried doing foreignKey: 'category.id' , but i forgot that we are referencing the way that it looks in the actual table 
+  foreignKey: 'category_id',
+});
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignKey: 'product_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: 'ProductTag',
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product,{
+  through: 'ProductTag',
+});
 module.exports = {
   Product,
   Category,
